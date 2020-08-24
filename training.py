ec732d249354040a4d0381b8b4965ff2252a30ea
hermanjakobsen <hermankjakobsen@gmail.com>
2020-08-24 19:25:39 +0200

    Able to run training pipeline
    There is still a bug with the gradient and parameter update. The test loss is not chaanging throughout training.

--- training.py
+++ training.py
@@ -3,43 +3,47 @@ from model import Model
 import torch
 import torch.optim as optim
 
-Epoch = 50
+Epoch = 10
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 xTrain, yTrain, xTest, yTest, yTestUnscaled = getTrainAndTestData("AAPL_daily.csv", 0.9)
 
-trainDataLoader = convertToDataLoader(xTrain, yTrain, 32, True, True)
-testDataLoader = convertToDataLoader(xTest, yTest, 32, False, False)
+trainDataLoader = convertToDataLoader(xTrain, yTrain, batchSize=32, shuffle=True, dropLast=True)
+testDataLoader = convertToDataLoader(xTest, yTest, batchSize=32, shuffle=False, dropLast=False)
 
-model = Model()
-model.to(device)
+model = Model().to(device)
 
-optimizer = optim.Adam(model.parameters(), lr=0.0005)
+optimizer = optim.Adam(model.parameters(), lr=0.05)
 
 for epoch in range(Epoch):
     print(f'Epoch: {epoch}')
     print(f'\tTraining...')
+    model.train()
 
     for x, y in trainDataLoader:
         x = x.to(device=device)
         y = y.to(device=device)
 
         logit = model(x)
+        loss =  model.computeLoss(logit[:,-1,:], y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    
+    print(f'\tTesting...')
+    with torch.no_grad():
+        model.eval()
+        testLoss = 0.
 
-        print(logit.shape)
+        for x, y in testDataLoader:
 
-        loss =  model.computeLoss(logit, y)
-        #accuracy = (logit.argmax(dim=1)==y).float().mean()
+            x = x.to(device=device)
+            y = y.to(device=device)
 
-        #optimizer.zero_grad()
+            logit = model(x)
+            loss =  model.computeLoss(logit[:,-1,:], y)
+            testLoss += loss
 
-        #loss.backward()
- 
-        #optimizer.step()
-
-
-        
-
-
+    print(f'Test loss for epoch {epoch}: {testLoss}')
 
 
