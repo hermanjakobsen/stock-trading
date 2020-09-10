37b0641d1f2d4248468cf6f199fe939965a452f1
hermanjakobsen <hermankjakobsen@gmail.com>
2020-09-10 17:45:44 +0200

    Rename model

--- training.py
+++ training.py
@@ -1,9 +1,9 @@
 from dataset import getTrainAndTestData, convertToDataLoader
-from model import Model
+from model import StockPredictor
 import torch
 import torch.optim as optim
 
-Epoch = 10
+Epoch = 4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 xTrain, yTrain, xTest, yTest, yTestUnscaled = getTrainAndTestData("AAPL_daily.csv", 0.9)
@@ -11,28 +11,30 @@ xTrain, yTrain, xTest, yTest, yTestUnscaled = getTrainAndTestData("AAPL_daily.cs
 trainDataLoader = convertToDataLoader(xTrain, yTrain, batchSize=32, shuffle=True, dropLast=True)
 testDataLoader = convertToDataLoader(xTest, yTest, batchSize=32, shuffle=False, dropLast=False)
 
-model = Model().to(device)
+stockPredictor = StockPredictor().to(device)
 
-optimizer = optim.Adam(model.parameters(), lr=0.05)
+optimizer = optim.Adam(stockPredictor.parameters(), lr=0.05)
 
 for epoch in range(Epoch):
     print(f'Epoch: {epoch}')
     print(f'\tTraining...')
-    model.train()
+    stockPredictor.train()
 
     for x, y in trainDataLoader:
+
         x = x.to(device=device)
         y = y.to(device=device)
+        
+        logit = stockPredictor(x)
 
-        logit = model(x)
-        loss =  model.computeLoss(logit[:,-1,:], y)
+        loss =  stockPredictor.computeLoss(logit, y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
     
     print(f'\tTesting...')
     with torch.no_grad():
-        model.eval()
+        stockPredictor.eval()
         testLoss = 0.
 
         for x, y in testDataLoader:
@@ -40,8 +42,8 @@ for epoch in range(Epoch):
             x = x.to(device=device)
             y = y.to(device=device)
 
-            logit = model(x)
-            loss =  model.computeLoss(logit[:,-1,:], y)
+            logit = stockPredictor(x)
+            loss =  stockPredictor.computeLoss(logit, y)
             testLoss += loss
 
     print(f'Test loss for epoch {epoch}: {testLoss}')
